815d0e35518d80dcf30d80e7679e6376a0d53cfa
donquixxy <agusary726@gmail.com>
2025-03-19 15:12:58 +0800

    init session id on open

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import HomePage from "./pages/home";
 import "./App.css";
 import { Toaster } from "./components/ui/toaster";
+import { useCookie } from "./hooks/cookies";
 
 function App() {
+  const session = useCookie();
+
+  session.setSessionID();
+
   return (
     <div className="background">
       <Toaster />
